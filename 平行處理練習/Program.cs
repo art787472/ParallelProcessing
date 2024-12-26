670ed55352bc447cbe7742168c59aabbbe071c5e
Chen Yi-Cheng <art787472@gmail.com>
2024-12-26 14:45:29 +0800

    加入專案檔案。

---
+++ 平行處理練習/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 平行處理練習
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
